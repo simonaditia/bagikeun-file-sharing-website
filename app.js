d8aa29fc013705d29fab34f9ea9b4b740eaae78e
simonaditia <smnsitumeang@gmail.com>
2023-09-22 19:22:25 +0700

    Download link endpoint

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require("express")
 const filesRoute = require("./routes/files")
 const showRoute = require("./routes/show")
+const downloadRoute = require("./routes/download")
 const app = express()
 const path = require("path")
 const PORT = 3001
@@ -15,6 +16,7 @@ app.set("view engine", "ejs")
 
 app.use("/api/files", filesRoute)
 app.use("/files", showRoute)
+app.use("/files/download", downloadRoute)
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
